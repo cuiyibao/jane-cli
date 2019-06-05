5607245ba6a999920a25611e38cf47ef835b6393
cuiyibao <cuiyibao001@ke.com>
2019-06-05 18:42:23 +0800

    引入有赞组件库

--- readme.md
+++ readme.md
@@ -2,14 +2,14 @@
  * @Description: 文件描述
  * @Author: yb001
  * @Date: 2019-06-03 11:19:14
- * @LastEditTime: 2019-06-03 15:19:31
+ * @LastEditTime: 2019-06-05 18:41:53
  * @LastEditors: yb001
  -->
 ## jane
 + [https://github.com/lin-hun/jane](https://github.com/lin-hun/jane)
 
 ## install
-+ npm install -g @linhun/jane-cli
++ npm install -g git+ssh://git@github.com:cuiyibao/jane-cli.git#master
 
 ## usage
 + jane --help
@@ -40,3 +40,25 @@ module.exports = (option)=>{
 * create.js, 删除创建模板后下载依赖的操作
 
 * tpl, 修改默认预处理方式为less
+
+* build.js, 文件编译添加按需引入依赖处理 (按需引入方式待优化)
+
+* compiler, 添加.json, .wxml, .wxs, .wxss的文件编译处理方法
+
+* tpl-project, 引入components组件库vant-weapp, pages添加组件模板
+
+* tpl-page, 添加index.json
+
+* jane.config.js, 变更ignore的dist为vant-weapp (2019-06-05)
+
+## TODO (by xb)
+
+* 按需引入方式优化
+
+* 页面按需引入，分离组件模板
+
+* tpl添加工具、拦截器（api管理）
+
+* tpl组件封装，定制化
+
+* 打包提供env环境识别
--- tpl/project/jane.config.js
+++ tpl/project/jane.config.js
@@ -2,28 +2,28 @@
  * @Description: 文件描述
  * @Author: yb001
  * @Date: 2019-06-03 11:19:14
- * @LastEditTime: 2019-06-03 15:17:57
+ * @LastEditTime: 2019-06-05 18:36:37
  * @LastEditors: yb001
  */
 module.exports = {
-  dest:'build',
-  tpl:{
-    page:'' // must be directory
+  dest: "build",
+  tpl: {
+    page: "" // must be directory
   },
   css: {
-    ext:'.less',
-    compiler: require('jane-less'),
-    config:{}
+    ext: ".less",
+    compiler: require("jane-less"),
+    config: {}
   },
   js: {
-    ext:'.js',
-    compiler: 'babel',
+    ext: ".js",
+    compiler: "babel",
     // https://babeljs.io/docs/usage/api/
     config: {
-      presets:['env'],
+      presets: ["env"],
       plugins: ["transform-node-env-inline"]
     }
   },
 
-  ignore:['node_modules','dist','.DB_store','.DS_Store']
+  ignore: ["node_modules", "vant-weapp", ".DB_store", ".DS_Store"]
 }
